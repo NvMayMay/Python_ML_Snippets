3890df4c4f22f4e260e34fab9db22eb5a3bc1a9f
admin <admin@admins-imac.home>
2025-09-05 11:48:52 +0200

    PCA updated to add variance plot

--- UNSUPERVISED_LEARNING/principal_component_analysis.py
+++ UNSUPERVISED_LEARNING/principal_component_analysis.py
@@ -79,3 +79,27 @@ plt.ylabel("PC2",)
 plt.legend(loc='best', shadow=False, scatterpoints=1,)
 plt.grid(True)
 #plt.show()
+
+# Show PCA model without reducing dimensions to see variance by components
+# Standardize the data
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
+# Apply PCA
+pca = PCA(n_components=4)
+X_pca = pca.fit_transform(X_scaled)
+explained_variance_ratio = pca.explained_variance_ratio_
+# Plot explained variance ratio for each component
+plt.figure(figsize=(10,6))
+plt.bar(x=range(1, len(explained_variance_ratio)+1), height=explained_variance_ratio, alpha=1, align='center', label='PC explained variance ratio' )
+plt.ylabel('Explained Variance Ratio')
+plt.xlabel('Principal Components')
+plt.title('Explained Variance by Principal Components')
+# Plot cumulative explained variance
+cumulative_variance = np.cumsum(explained_variance_ratio)
+plt.step(range(1, 5), cumulative_variance, where='mid', linestyle='--', lw=3,color='red', label='Cumulative Explained Variance')
+# Only display integer ticks on the x-axis
+plt.xticks(range(1, 5))
+plt.legend()
+plt.grid(True)
+plt.show()
+# shows how important each principal component is in explaining variance in the data
