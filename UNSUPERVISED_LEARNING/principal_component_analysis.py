2e7e0b66cad6fcf9ccb5cc4e8c6386ee24c508b0
admin <admin@admins-imac.home>
2025-09-05 11:40:11 +0200

    PCA added

---
+++ UNSUPERVISED_LEARNING/principal_component_analysis.py
@@ -0,0 +1,81 @@
+# install relevant libraries
+# python3 -m pip install numpy matplotlib scikit-learn
+
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+from sklearn import datasets
+from sklearn.preprocessing import StandardScaler
+
+# Create a 2D dataset with 2 linearly correlated features
+# Use bivariate normal distribution to generate the dataset
+# both features will have mean=0 and a covariance given by covariance matrix [[3,2][2,2]]
+np.random.seed(42)
+mean = [0, 0]
+cov = [[3, 2], [2, 2]]
+X = np.random.multivariate_normal(mean=mean, cov=cov, size=200)
+
+# Scatter plot of the two features
+# Delete hash in below section to print
+plt.figure()
+plt.scatter(X[:, 0], X[:, 1],  edgecolor='k', alpha=0.7)
+plt.title("Scatter Plot of Bivariate Normal Distribution")
+plt.xlabel("X1")
+plt.ylabel("X2")
+plt.axis('equal')
+plt.grid(True)
+# plt.show()
+
+# Initialise 2-component PCA model
+pca = PCA(n_components=2)
+X_pca = pca.fit_transform(X)
+# get principal components (principal axes in feature space that align with direcitons of maximum variance in data)
+components = pca.components_
+#projection of data onto principal components yields coordinates of data along principal axes
+# This is the dot product of each data point with each principal component
+projection_pc1 = np.dot(X, components[0])
+projection_pc2 = np.dot(X, components[1])
+x_pc1 = projection_pc1 * components[0][0]
+y_pc1 = projection_pc1 * components[0][1]
+x_pc2 = projection_pc2 * components[1][0]
+y_pc2 = projection_pc2 * components[1][1]
+plt.figure()
+plt.scatter(X[:, 0], X[:, 1], label='Original Data', ec='k', s=50, alpha=0.6)
+# Plot the projections along PC1 and PC2, delete hash in below lines for output
+plt.scatter(x_pc1, y_pc1, c='r', ec='k', marker='X', s=70, alpha=0.5, label='Projection onto PC 1')
+plt.scatter(x_pc2, y_pc2, c='b', ec='k', marker='X', s=70, alpha=0.5, label='Projection onto PC 2')
+plt.title('Linearly Correlated Data Projected onto Principal Components', )
+plt.xlabel('Feature 1',)
+plt.ylabel('Feature 2',)
+plt.legend()
+plt.grid(True)
+plt.axis('equal')
+#plt.show()
+
+#Now use PCA to project 4 dimenstional Iris dataset onto 2 dimensions
+# Load the Iris dataset
+iris = datasets.load_iris()
+X = iris.data
+y = iris.target
+target_names = iris.target_names
+# Standardize the data
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
+# Get flower (target) names
+iris.target_names
+# Apply PCA and reduce the dataset to 2 components
+pca = PCA(n_components=2)
+X_pca = pca.fit_transform(X_scaled)
+# Plot the PCA-transformed data in 2D, delete hash in below lines for output
+plt.figure(figsize=(8,6))
+colors = ['navy', 'turquoise', 'darkorange']
+lw = 1
+for color, i, target_name in zip(colors, [0, 1, 2], target_names):
+    plt.scatter(X_pca[y == i, 0], X_pca[y == i, 1], color=color, s=50, ec='k',alpha=0.7, lw=lw,
+                label=target_name)
+plt.title('PCA 2-dimensional reduction of IRIS dataset',)
+plt.xlabel("PC1",)
+plt.ylabel("PC2",)
+plt.legend(loc='best', shadow=False, scatterpoints=1,)
+plt.grid(True)
+#plt.show()
